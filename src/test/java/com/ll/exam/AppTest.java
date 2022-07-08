e6b60fe4c6064dfc8d2f310d8b58a033331382e6
EunJung803 <aimee3322@gmail.com>
2022-07-08 14:44:38 +0900

    TDD / 표준출력 리다이렉션, 출력을 문자열로 받기

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,8 +2,7 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
 
     @Test
-    public void scannerTest() {
+    public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
                 등록
                 명언1
@@ -27,5 +26,22 @@ public class AppTest {
         assertEquals("등록", cmd1);
         assertEquals("명언1", quote1);
         assertEquals("작가1", author1);
+    }
+
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        //표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        String rs = output.toString().trim();
+
+        //표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
     }
 }
