244dd50b64db8a4629b12fb1e3d1eaa9951f4d85
EunJung803 <aimee3322@gmail.com>
2022-07-12 17:02:46 +0900

    WiseSayingRepository, WiseSayingController 수정 / 컨트롤러에 세부 역할 하위 기관에 위임

--- src/main/java/com/ll/exam/WiseSayingController.java
+++ src/main/java/com/ll/exam/WiseSayingController.java
@@ -1,6 +1,5 @@
 package com.ll.exam;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,10 +31,12 @@ public class WiseSayingController {
 
         System.out.printf("명언(기존) : %s\n", foundWiseSaying.quote);  // 기존의 명언을 보여주고
         System.out.printf("명언 : ");
-        foundWiseSaying.quote = sc.nextLine();  // 수정할 명언을 scanner로 받아 바로 foundWiseSaying.quote 부분에 저장
+        String quote = sc.nextLine();  // 수정할 명언을 scanner로 받아 바로 foundWiseSaying.quote 부분에 저장
         System.out.printf("작가(기존) : %s\n", foundWiseSaying.author);  // 기존의 작가를 보여주고
         System.out.printf("작가 : ");
-        foundWiseSaying.author = sc.nextLine();  // 수정할 작가명을 scanner로 받아 바로 foundWiseSaying.author 부분에 저장
+        String author = sc.nextLine();  // 수정할 작가명을 scanner로 받아 바로 foundWiseSaying.author 부분에 저장
+
+        wiseSayingRepository.modify(paramID, quote, author);
 
         System.out.printf("%d번 명언이 수정되었습니다.\n", paramID);
     }
@@ -47,11 +48,9 @@ public class WiseSayingController {
         System.out.printf("작가 : ");
         String author = sc.nextLine().trim();   // 작가 입력받기
 
-        int id = ++wiseSayingRepository.id;
-        System.out.println(id + "번 명언이 등록되었습니다.");
+        WiseSaying wiseSaying = wiseSayingRepository.write(quote, author);
 
-        WiseSaying wiseSaying = new WiseSaying(id, quote, author);  // WiseSaying 생성
-        wiseSayingRepository.wiseList.add(wiseSaying);   // WiseSaying List에 WiseSaying 추가
+        System.out.println(wiseSaying.id + "번 명언이 등록되었습니다.");
     }
 
     public void remove(Rq rq) {
@@ -71,7 +70,7 @@ public class WiseSayingController {
             return;
         }
 
-        wiseSayingRepository.wiseList.remove(foundWiseSaying);  // 입력된 id에 해당하는 명언객체(== 담아뒀던 명언)를 리스트에서 삭제하기
+        wiseSayingRepository.remove(paramID);  // 입력된 id에 해당하는 명언객체(== 담아뒀던 명언)를 리스트에서 삭제하기
         System.out.printf("%d번 명언이 삭제되었습니다.\n", paramID);
     }
 
@@ -79,8 +78,10 @@ public class WiseSayingController {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------");
 
-        for(int i = wiseSayingRepository.wiseList.size()-1; i >= 0; i--) {
-            WiseSaying wiseList_ = wiseSayingRepository.wiseList.get(i);
+        List<WiseSaying> wiseSayings = wiseSayingRepository.findAll();
+
+        for(int i = wiseSayings.size()-1; i >= 0; i--) {
+            WiseSaying wiseList_ = wiseSayings.get(i);
             System.out.printf("%d / %s / %s\n", wiseList_.id, wiseList_.author, wiseList_.quote);
         }
     }
--- src/main/java/com/ll/exam/WiseSayingRepository.java
+++ src/main/java/com/ll/exam/WiseSayingRepository.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class WiseSayingRepository {
 
-    public int id;
-    public List<WiseSaying> wiseList;
+    private int id;
+    private List<WiseSaying> wiseList;
 
     WiseSayingRepository() {
         wiseList = new ArrayList<>();
@@ -23,5 +23,39 @@ public class WiseSayingRepository {
 
         // 다 돌았는데 없으면 null
         return null;
+
+        // 하나 찾기
     }
+
+    public List<WiseSaying> findAll() {
+        return wiseList;
+
+        // 다 찾기
+    }
+
+    public WiseSaying write(String content, String author) {
+        int wiseid = ++id;
+        WiseSaying wiseSaying = new WiseSaying(wiseid, content, author);
+        wiseList.add(wiseSaying);
+
+        // 파일저장
+
+        return wiseSaying;
+    }
+
+    public void remove(int paramId) {
+        WiseSaying foundWiseSaying = findById(paramId);
+        wiseList.remove(foundWiseSaying);
+
+        // 파일 삭제
+    }
+
+    public void modify(int paramId, String content, String author) {
+        WiseSaying foundWiseSaying = findById(paramId);
+        foundWiseSaying.quote = content;
+        foundWiseSaying.author = author;
+
+        // 파일 수정
+    }
+
 }
