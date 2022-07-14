baf39a6f9486fdfd1fafa0922a842c2d2739e3cf
EunJung803 <aimee3322@gmail.com>
2022-07-15 02:45:29 +0900

    FileDBTest - beforeEach 도입

--- src/test/java/com/ll/exam/FileDBTest.java
+++ src/test/java/com/ll/exam/FileDBTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -7,10 +8,14 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileDBTest {
-    @Test
-    void 파일에_있는_JSON을_객체로_변환() {
+
+    @BeforeEach     // 각 테스트 전에 미리 수행해주는 역할 ( 매 테스트를 시작하면서 deleteDir, mkdir을 해줄 필요가 없어졌다 이제 )
+    void beforeEach() {
         Util.deleteDir("test_data");
         Util.mkdir("test_data");
+    }
+    @Test
+    void 파일에_있는_JSON을_객체로_변환() {
         WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
         Util.saveToFile("test_data/1.json", wiseSaying.toJson());  // toJson 메서드 실행하기
 
@@ -25,8 +30,6 @@ public class FileDBTest {
 
     @Test
     void 파일에_있는_JSON을_맵으로_변환() {
-        Util.deleteDir("test_data");
-        Util.mkdir("test_data");
         WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
         Util.saveToFile("test_data/1.json", wiseSaying.toJson());  // toJson 메서드 실행하기
 
@@ -40,8 +43,6 @@ public class FileDBTest {
 
     @Test
     void 파일에_객체를_저장() {
-        Util.deleteDir("test_data");
-        Util.mkdir("test_data");
         WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
         Util.saveToFile("test_data/1.json", wiseSaying.toJson());  // toJson 메서드 실행하기
 
@@ -51,8 +52,6 @@ public class FileDBTest {
 
     @Test
     void 파일에_내용쓰기() {
-        Util.deleteDir("test_data");
-        Util.mkdir("test_data");  // 폴더 생성
         Util.saveToFile("test_data/1.json", "내용\n내용");  // json 파일 생성 / body == 내용
 
         String rs = Util.readFromFile("test_data/1.json");
