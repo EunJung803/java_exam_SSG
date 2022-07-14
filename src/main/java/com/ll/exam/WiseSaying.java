931e4db7527878019b38da4825358b83058a8478
EunJung803 <aimee3322@gmail.com>
2022-07-14 14:26:25 +0900

    객체를 파일에 저장 / toJson 메서드 구현

--- src/main/java/com/ll/exam/WiseSaying.java
+++ src/main/java/com/ll/exam/WiseSaying.java
@@ -20,4 +20,16 @@ public class WiseSaying {
                 '}';
     }
 
+    public String toJson() {  // Json 형태로 변환해주기
+        return """
+                {
+                    "id": %d,
+                    "content": "%s",
+                    "author": "%s"
+                }
+                """
+                .stripIndent()      // 문자열 안의 모든 라인에 strip를 적용하여 앞 뒤 공백을 제거
+                .formatted(id, quote, author)       // 문자열의 형식 지정
+                .trim();        // 문자열의 앞&뒤 공백 제거
+    }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -8,6 +8,15 @@ import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+    @Test
+    void 파일에_객체를_저장() {
+        Util.mkdir("test_data");
+        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Util.saveToFile("test_data/1.json", wiseSaying.toJson());  // toJson 메서드 실행하기
+
+        String rs = Util.readFromFile("test_data/1.json");
+        assertEquals(wiseSaying.toJson(), rs);
+    }
 
     @Test
     void 파일에_내용쓰기() {
